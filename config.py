6af2f35f524d07a9b99cea7105d667cc4e360dc2
Pakkapon Phongthawee <me@pureapp.in.th>
2019-11-15 12:44:36 +0700

    save: runable!

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-#Windows Setting
+""" Windows Setting """
 WINDOW_TITLE = 'Pong - Face Control - VisionLab@VISTEC'
-WINDOW_HEIGHT = 360
-WINDOW_WIDTH =  640
 
-# Camera Setting
-CAMERA_HEIGHT = 360
+""" Camera Setting """
+CAMERA_HEIGHT = 480
 CAMERA_WIDTH = 640
 
-#Game Settings
+""" Game Settings """
 BOARD_WIDTH = 40
 BOARD_HEIGHT = 30
-PADDLE_HEIGHT = 4
+PADDLE_HEIGHT = 5
+SPEED_STEP = 0.1
+# increase wait time (milisecond ) if PRNet can't find face
+# to make game run as same speed
+WAIT_TIME = 30 
--- pong.py
+++ pong.py
@@ -3,15 +3,18 @@ import numpy as np
 import cv2,config
 from utils.cv_plot import plot_kpt, plot_vertices, plot_pose_box
 from utils.estimate_pose import estimate_pose
-from game.pong_game import Pong
-
+from game_engine.pong_game import PongGame
 
 prn = PRN(is_dlib = True)
+pong_game = PongGame()
 cap = cv2.VideoCapture(0)
+
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, config.CAMERA_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, config.CAMERA_HEIGHT)
-
+COLOR_UP = (0,255,0)
+COLOR_DOWN = (0,0,255)
+COLOR_STAY = (0,255,255)
 def head_pose(frame):
     global prn
     try:
@@ -19,11 +22,35 @@ def head_pose(frame):
         kpt = prn.get_landmarks(pos)
         vertices = prn.get_vertices(pos)
         camera_matrix, pose = estimate_pose(vertices)
-        frame = plot_pose_box(frame, camera_matrix, kpt)
+        if pose[1] > config.SPEED_STEP:
+            color = COLOR_UP
+        elif pose[1] < - config.SPEED_STEP:
+            color = COLOR_DOWN
+        else:
+            color = COLOR_STAY
+        frame = plot_pose_box(frame, camera_matrix, kpt, color=color)
     except:
         pose = (0.0,0.0,0.0)
     return frame, pose
+def speed_step(s):
+    step = config.SPEED_STEP
+    s = -s
+    if s > step * 3:
+        return 3
+    if s > step * 2:
+        return 2
+    if s > step:
+        return 1
+    if s > step * -1:
+        return 0
+    if s > step * -2:
+        return -1
+    if s > step * -3:
+        return -2
+    else:
+        return -3
 
+is_play_game = False
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -36,10 +63,23 @@ while(True):
     frame[:,half_width:], pose_right = head_pose(frame[:,half_width:])
 
     cv2.line(frame, (half_width, 0), (half_width, config.CAMERA_HEIGHT), (0,255,0), 2)
+    # Pong Game Control
+    if is_play_game:
+        pong_game.make_move(speed_step(pose_left[1]),speed_step(pose_right[1]))
+    pong_game.draw(frame)
     # Display the resulting frame
     cv2.imshow(config.WINDOW_TITLE,frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    waitTime = 1
+    if pose_left[1] == 0.0 and pose_right[1] == 0.0:
+        waitTime = 30
+    keyboard = cv2.waitKey(waitTime)
+    if keyboard & 0xFF == ord('q'):
         break
+    if keyboard & 0xFF == ord('r'):
+        pong_game.reset_game()
+        is_play_game = False
+    if keyboard & 0xFF == ord('p'):
+        is_play_game = not is_play_game
 
 # When everything done, release the capture
 cap.release()
